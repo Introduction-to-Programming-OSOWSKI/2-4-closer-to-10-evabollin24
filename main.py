609c9ba91d04c0794bbaca8f806ceeff8581baab
evabollin24 <817972@mystma.org>
2021-09-28 09:25:30 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x, y):
+    if (10 - x) < (10 - y):
+        return abs(x)
+    elif (10 - x) > (10 - y):
+        return abs(y)
+    else:
+        return abs(0)
