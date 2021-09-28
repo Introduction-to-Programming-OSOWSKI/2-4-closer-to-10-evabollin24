38ead5c1c6e6e7ed63a0e933bcc6cac4711ff4c9
evabollin24 <817972@mystma.org>
2021-09-28 09:34:53 -0500

    3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def close10(x, y):
-    if (10 - x) < (10 - y):
+    if (10 - x) < (y - 10):
         return abs(x)
-    elif (10 - x) > (10 - y):
+    elif (10 - x) > (y - 10):
         return abs(y)
     else:
         return 0
+print(close10(5, 12))
